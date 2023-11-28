e32775f82671a3b751db0e5c8036d4d56c3df6c7
Garx1212 <113130182+Garx1212@users.noreply.github.com>
2023-11-28 03:27:08 +0000

    commit

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,37 +1,26 @@
 import React from "react";
 
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid d">
-    <a class="navbar-brand" href="#">Navbar</a>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand d-flex justify-content-start" href="#">Start Bootstrap</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
+      <div className="d-flex justify-content-end">
+      <div class="navbar-nav d-flex justify-content-end ">
         <a class="nav-link active" aria-current="page" href="#">Home</a>
         <a class="nav-link" href="#">Features</a>
         <a class="nav-link" href="#">Pricing</a>
         <a class="nav-link disabled" aria-disabled="true">Disabled</a>
       </div>
+      </div>
     </div>
   </div>
 </nav>
-
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
 	);
 };
 
-export default Home;
+export default Navbar;
